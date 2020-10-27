aaee37e860cc9e141ff39854188e2b39733bd40d
chromohh <sindre.he@hotmail.com>
2020-10-27 12:16:21 +0100

    minor edits

---
+++ src/main/java/com/sindre/inl2/demo/services/NyUserDetailService.java
@@ -0,0 +1,39 @@
+package com.sindre.inl2.demo.services;
+
+import com.sindre.inl2.demo.entities.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class NyUserDetailService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
+        User user = userService.findByUsername(name);
+        if(user == null) {
+            throw new UsernameNotFoundException("Username " + name + " not found");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), getGrantedAuthorities(user));
+    }
+
+
+    private Collection<GrantedAuthority> getGrantedAuthorities(User user) {
+        return user.getAcl().stream()
+                .map(authority -> new SimpleGrantedAuthority("ROLE_" + authority))
+                .collect(Collectors.toList());
+    }
+}
